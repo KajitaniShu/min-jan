c849e1a0e8bef4e5e71d9a7366e7d2594494cdf7
shu kajitani <hiiragi.univ@gmail.com>
2023-12-14 16:19:07 +0900

    Fix ユーザーページにアクセスするとエラーが表示されるバグを修正

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,7 +4,7 @@ import multiavatar from '@multiavatar/multiavatar'
 import { IconRefresh } from '@tabler/icons-react';
 
 export function Profile({userData}: any) {
-
+  console.log(userData)
   return (
     <Container>
       <Group mb="xl">
@@ -12,8 +12,8 @@ export function Profile({userData}: any) {
         <Text size="xl" fw="bolder" c="dark.4">ユーザー情報</Text>
       </Group>
       <Paper withBorder radius="md" shadow="xs" py="lg" px="xl">
-        <Text size="xs" c="dimmed" mt="md" mb="xs">ニックネーム</Text>
-        <Input size="md" variant="filled" placeholder={userData[0].name} />
+        <Text size="xs" c="dimmed" mb="xs">ニックネーム</Text>
+        <Input size="md" variant="filled" placeholder={userData && userData.length > 0 && userData[0].name} />
       </Paper>
       <Button mt="xl" bg="black" size="md" fullWidth>変更する</Button>
     </Container>
